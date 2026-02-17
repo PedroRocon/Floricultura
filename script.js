68c7cb74ee40838e4e92a8caf5daba32bf0049c8
PedroRocon <pedrohrp2506@gmail.com>
2026-02-16 22:29:13 -0300

    slider pc feito

--- script.js
+++ script.js
@@ -4,3 +4,31 @@ function Mostrarinfo(){
         info.classList.toggle("ativa")
     });
 }
+
+let currentSlide = 1;
+
+startslider();
+
+function startslider(){
+    setInterval(function(){
+        nextslide();
+    }, 5000)
+}
+
+function nextslide(){
+    currentSlide++;
+
+    if (currentSlide > 4){
+        currentSlide = 1;
+    }
+
+    showslide(currentSlide);
+}
+
+function showslide(index){
+    for (let i = 1; i <= 4; i++){
+        document.getElementById("r" + i).checked = false
+    }
+    document.getElementById("r" + index).checked = true
+}
+
